cf5c4ba3be324d5bd73c956b3b70012f14feba4e
Chris Ruppel <ruppel@reliefweb.int>
2022-10-07 14:51:12 +0200

    feat: minimum installable PWA
    Refs: NUM-4

---
+++ html/themes/custom/common_design_subtheme/js/service-worker-registration.js
@@ -0,0 +1,7 @@
+(function iife() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js');
+    });
+  }
+})();
