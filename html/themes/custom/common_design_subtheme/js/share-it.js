24646e0d3769e4a5c65add59673da992e1cf0b49
Chris Ruppel <ruppel@reliefweb.int>
2022-11-25 07:54:49 +0100

    fix: error handling for share script
    Refs: NUM-14

--- html/themes/custom/common_design_subtheme/js/share-it.js
+++ html/themes/custom/common_design_subtheme/js/share-it.js
@@ -1,72 +1,71 @@
 /**
  * @file
- * Attach destination to login link.
+ * Use native browser share mechanism on individual figures.
  */
 
- (function () {
-  if (typeof navigator.share !== 'function') {
-    // No support.
-    //return;
-  }
+(function () {
+  if (!!navigator.canShare) {
+    var keyFigures = document.querySelectorAll('.rw-key-figures__figure');
 
-  var keyFigures = document.querySelectorAll('.rw-key-figures__figure');
-  if (!keyFigures) {
-    // No figures found.
-    return;
-  }
+    keyFigures.forEach((keyfigure) => {
+      // Attach share button.
+      let button = document.createElement('button');
+      button.innerHTML = 'Share';
+      button.classList.add('rw-key-figures__share-button');
 
-  keyFigures.forEach(keyfigure => {
-    // Attach share button.
-    let button = document.createElement('button');
-    button.innerHTML = 'Share';
-    button.classList.add('rw-key-figures__share-button');
+      button.addEventListener('click', (ev) => {
+        let keyFigure = ev.target.parentElement;
 
-    button.addEventListener('click', e => {
-      let keyFigure = e.target.parentElement;
+        let shareTitle = window.title;
+        let shareUrl = window.location.href + '#' + keyFigure.id;
+        let sharetext = [
+          keyFigure.querySelector('.rw-key-figures__figure__label').innerHTML,
+          ': ',
+          keyFigure.querySelector('.rw-key-figures__figure__value').innerHTML
+        ].join('');
 
-      let shareTitle = window.title;
-      let shareUrl = window.location.href + '#' + keyFigure.id;
-      let sharetext = [
-        keyFigure.querySelector('.rw-key-figures__figure__label').innerHTML,
-        ': ',
-        keyFigure.querySelector('.rw-key-figures__figure__value').innerHTML
-      ].join('');
-
-      try {
         // Hide share button.
         keyFigure.querySelector('.rw-key-figures__share-button').style.display = 'none';
 
         // Try sharing image.
-        html2canvas(keyFigure).then(canvas => {
+        html2canvas(keyFigure).then((canvas) => {
+          // Attempt to create image from DOM.
           canvas.toBlob(async (blob) => {
-            const images = [new File([blob], 'image.png', { type: blob.type })];
-            navigator.share({
-              title: shareTitle,
-              text: sharetext,
-              url: shareUrl,
-              files: images
-            })
+            try {
+              // Attempt to share image.
+              const images = [new File([blob], 'image.png', { type: blob.type })];
+              navigator.share({
+                title: shareTitle,
+                text: sharetext,
+                url: shareUrl,
+                files: images
+              }).catch(error => {
+                // If the first share attempt failed, we will log it but try again
+                // with plain text before reporting failure to the user.
+                console.log('Image sharing failed:', error);
+
+                // Attempt to share text.
+                navigator.share({
+                  title: shareTitle,
+                  text: sharetext,
+                  url: shareUrl
+                }).catch(error => {
+                  button.innerHTML = 'Share failed...';
+                  console.log('Text sharing failed:', error);
+                });
+              });
+            } catch (err) {
+              console.error('Sharing failed:', err);
+            }
           });
         });
-      } catch (error) {
-        // Share as text.
-        try {
-          navigator.share({
-            title: shareTitle,
-            text: sharetext,
-            url: shareUrl
-          })
-        } catch (error) {
-          console.log('Sharing failed!', error)
-        }
-      }
 
-      // Show share button.
-      keyFigure.querySelector('.rw-key-figures__share-button').style.display = 'inherit';
+        // Show share button.
+        keyFigure.querySelector('.rw-key-figures__share-button').style.display = 'block';
+      });
+
+      keyfigure.append(button);
     });
-
-    keyfigure.append(button);
-  });
-
+  }
 }());
 
