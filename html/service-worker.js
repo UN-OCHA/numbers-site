e7136024a9eaad5f7b320a7bfb2caafadc486bc3
Chris Ruppel <ruppel@reliefweb.int>
2022-12-01 10:43:01 +0100

    chore: attempt a new SW to view non-prod envs on phones
    Since they have basic auth we need to bypass it.
    
    Refs: NUM-14

--- html/service-worker.js
+++ html/service-worker.js
@@ -1,3 +1,8 @@
-self.addEventListener('fetch', () => {
-  return;
+self.addEventListener('fetch', (event) => {
+  // Ensure we can bypass basic auth in non-prod environments.
+  const modifiedRequest = new Request(event.request, {credentials: 'same-origin'});
+
+  event.respondWith((async () => {
+    return fetch(modifiedRequest);
+  })());
 });
