5e97591b489582d14cc755275b9875a5e76e3138
Chris Ruppel <ruppel@reliefweb.int>
2023-07-13 09:09:11 +0200

    test: run JS ilnter

--- html/themes/custom/common_design_subtheme/js/service-worker-registration.js
+++ html/themes/custom/common_design_subtheme/js/service-worker-registration.js
@@ -1,4 +1,6 @@
-(function iife() {
+(function () {
+  'use strict';
+
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker.register('/service-worker.js');
